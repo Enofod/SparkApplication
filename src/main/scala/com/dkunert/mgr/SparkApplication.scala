7a8bfd55c42720bf9f5d35a15c1e4c2dcfadfcfe
Dawid Kunert <enofod@gmail.com>
2018-08-28 23:05:30 +0200

    From tutorial

--- src/main/scala/com/dkunert/mgr/SparkApplication.scala
+++ src/main/scala/com/dkunert/mgr/SparkApplication.scala
@@ -1,16 +1,77 @@
 package com.dkunert.mgr
 
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.ml.evaluation.ClusteringEvaluator
+import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler}
+import org.apache.spark.sql.SparkSession
 
 object SparkApplication {
   def main(args:Array[String]): Unit = {
-    val sparkConfig = new SparkConf()
-    sparkConfig.setMaster("local")
-    sparkConfig.setAppName("First application")
+    val spark = SparkSession.builder()
+      .appName("Titatnic app")
+      .config("spark.master", "local")
+      .getOrCreate();
 
-    val sparkContext = new SparkContext(sparkConfig)
+    val rawData = spark.read
+      .format("csv")
+      .option("header", "true")
+      .load("F:\\bigdata\\titanic\\all\\train.csv")
 
-    val rdd1 = sparkContext.makeRDD(Array(1,2,3,4,5,6))
-    rdd1.collect().foreach(println)
+    rawData.printSchema()
+    rawData.head(5).foreach(println);
+
+    var dataset = rawData.select(
+      rawData("Survived").cast("float"),
+      rawData("Pclass").cast("float"),
+      rawData("Sex"),
+      rawData("Age").cast("float"),
+      rawData("Fare").cast("float"),
+      rawData("Embarked")
+    )
+
+    dataset = dataset.na.drop()
+
+    dataset.head(5).foreach(println)
+
+    dataset = new StringIndexer()
+      .setInputCol("Sex")
+      .setOutputCol("Gender")
+      .setHandleInvalid("keep")
+      .fit(dataset).transform(dataset)
+
+    dataset = new StringIndexer()
+      .setInputCol("Embarked")
+      .setOutputCol("Boarded")
+      .setHandleInvalid("keep")
+      .fit(dataset).transform(dataset)
+
+    dataset = dataset.drop("Sex")
+    dataset = dataset.drop("Embarked")
+    dataset.head(5).foreach(println)
+
+    val requiredFeatures = Array("Survived", "Pclass", "Age", "Fare", "Gender", "Boarded")
+
+    val assembler = new VectorAssembler()
+      .setInputCols(requiredFeatures)
+      .setOutputCol("features")
+
+    val transformedData = assembler.transform(dataset)
+
+    transformedData.head(5).foreach(println)
+
+    val kmeans = new KMeans()
+      .setK(5)
+      .setSeed(1)
+
+    val model = kmeans.fit(transformedData)
+
+    val clusteredData = model.transform(transformedData)
+
+    val evaluator = new ClusteringEvaluator()
+    val silhouette = evaluator.evaluate(clusteredData)
+
+    println("Silhouette: ", silhouette)
+
+    model.clusterCenters.foreach(println)
   }
 }
