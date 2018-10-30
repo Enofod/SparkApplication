8c893f7a919375d21baadb46f38ec15c22277401
Dawid Kunert <enofod@gmail.com>
2018-10-30 17:32:01 +0100

    Remove indexer

--- src/main/scala/com/dkunert/mgr/classification/pipeline/PipelineFactory.scala
+++ src/main/scala/com/dkunert/mgr/classification/pipeline/PipelineFactory.scala
@@ -1,6 +1,5 @@
 package com.dkunert.mgr.classification.pipeline
 
-import org.apache.spark.ml.feature.VectorIndexer
 import org.apache.spark.ml.{Pipeline, PipelineStage}
 import org.apache.spark.sql.DataFrame
 
@@ -13,15 +12,8 @@ object PipelineFactory {
 
   def getPipeline(algorithm: PipelineStage, data: DataFrame): Pipeline = {
 
-    val indexer = new VectorIndexer()
-      .setInputCol(UNPROCESSED_FEATURES_KEY)
-      .setOutputCol(FEATURES_KEY)
-      .setMaxCategories(12)
-      .fit(data)
-
-
     val pipeline = new Pipeline()
-      .setStages(Array(indexer, algorithm))
+      .setStages(Array(algorithm))
 
     return pipeline
   }
